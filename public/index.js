a2997e31c42c9a2773a9c0be8fa95c92e43132cb
Ramona Snider <41329897+rlsnider@users.noreply.github.com>
2022-08-09 20:04:06 -0700

    added fetch/counter

--- public/index.js
+++ public/index.js
@@ -1,9 +1,11 @@
-function main(){
+async function main(){
     const countContainer = document.querySelector('#count-container');
     const incrementButton = document.querySelector('#increment-button');
     const decrementButton = document.querySelector('#decrement-button');
-
-    let countValue = 0;
+    let res = await fetch('http://127.0.0.1:9001/counter')
+    let data = await res.json()
+    console.log(data)
+    let countValue = data.value;
 
     function increment(){
         countValue++;
